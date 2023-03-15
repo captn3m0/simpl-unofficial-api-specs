6d4172e61c7181fefd038ed16f853ba36d6d3dba
Nemo <commits@captnemo.in>
2023-03-15 20:52:19 +0530

    fix link

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   if (window.location.hash) {
     url = window.location.hash.substr(2)
   } else {
-    url = "reference/profile.yaml"
+    url = "reference/Simpl.yaml"
   }
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
